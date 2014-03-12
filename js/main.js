adfe6190ab61012691ceecd93841a7732bffd3aa
Rishat Kadyrov <rishat.kadyrov@gmail.com>
2014-03-12 20:27:24 +0400

    Все линии метро. Есть прикольный баг с Питерским

--- js/main.js
+++ js/main.js
@@ -1,34 +1,6 @@
-var geocoder;
 var map;
-var metroCoordinates = {};
-
-var stations = [
-    'Алма-Атинская',
-    'Красногвардейская',
-    'Домодедовская',
-    'Орехово',
-    'Царицыно',
-    'Кантемировская',
-    'Каширская',
-    'Коломенская',
-    'Автозаводская',
-    'Павелецкая',
-    'Новокузнецкая',
-    'Театральная',
-    'Тверская',
-    'Маяковская',
-    'Белорусская',
-    'Динамо',
-    'Аэропорт',
-    'Сокол',
-    'Войковская',
-    'Водный стадион',
-    'Речной вокзал'
-];
-
 
 function initialize() {
-    geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(55.5, 38);
     var mapOptions = {
         zoom: 8,
@@ -37,37 +9,33 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 
-    var flightPlanCoordinates = [];
+    allStations.forEach(function (value) {
 
-    allStations[0]['stations'].forEach(function (value) {
-        flightPlanCoordinates.push(new google.maps.LatLng(value.lat, value.lng));
+        var flightPlanCoordinates = [];
+
+        value['stations'].forEach(function (value) {
+            flightPlanCoordinates.push(new google.maps.LatLng(value.lat, value.lng));
+        });
+
+        var flightPath = new google.maps.Polyline({
+            path: flightPlanCoordinates,
+            geodesic: true,
+            strokeColor: value['color'],
+            strokeOpacity: 1.0,
+            strokeWeight: 5
+        });
+        flightPath.setMap(map);
     });
-
-    var flightPath = new google.maps.Polyline({
-        path: flightPlanCoordinates,
-        geodesic: true,
-        strokeColor: allStations[0]['color'],
-        strokeOpacity: 1.0,
-        strokeWeight: 2
-    });
-    flightPath.setMap(map);
-
-
-
-    /*stations.forEach(function (value, index) {
-        setTimeout(function () {
-            codeAddress('Метро ' + value, value);
-        }, index * 700);
-    });*/
 }
 
 function codeAddress(fullAddress, address) {
     geocoder.geocode({ 'address': fullAddress}, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            metroCoordinates[address] = {
+            metroCoordinates.push ({
+                name: address,
                 lat: results[0].geometry.location.lat(),
                 lng: results[0].geometry.location.lng()
-            };
+            });
             console.log(JSON.stringify(metroCoordinates));
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
