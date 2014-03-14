62e843e8e3828ce7e76faf002e70b05e9fe4a482
Rishat Kadyrov <rishat.kadyrov@gmail.com>
2014-03-14 19:31:19 +0400

    Первая бета-версия

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
 var map;
+var geocoder;
 
-function initialize() {
-    var center = {
-        "lat": 45.752289,
+function Map() {
+    geocoder = new google.maps.Geocoder();
+
+    window.center = {
+        "lat": 55.752289,
         "lng": 37.60866299999998
     };
 
-    var metroCenter = {
+    window.metroCenter = {
         "lat": 55.752289,
         "lng": 37.60866299999998
     };
 
     var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(55.5, 38)
+        zoom: 11,
+        center: new google.maps.LatLng(55.752289, 37.60866299999998)
     };
+
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    console.log(this);
+    drawMetro();
 
-    var latDelta = metroCenter.lat - center.lat;
-    var lngDelta = metroCenter.lng - center.lng;
+}
 
+function drawMetro () {
+    console.log(123);
+    var latDelta = window.metroCenter.lat - window.center.lat;
+    var lngDelta = window.metroCenter.lng - window.center.lng;
 
     allStations.forEach(function (value) {
         var flightPlanCoordinates = [];
-
         value['stations'].forEach(function (value) {
             flightPlanCoordinates.push(new google.maps.LatLng(
                 value.lat + latDelta,
@@ -40,6 +48,23 @@ function initialize() {
         });
         flightPath.setMap(map);
     });
+};
+
+
+function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            window.metroCenter = {
+                "lat": results[0].geometry.location.lat(),
+                "lng": results[0].geometry.location.lng()
+            };
+            drawMetro();
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', Map);
